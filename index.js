f05f4f73b005e37a2999b0aaf6a99a72220ce3c4
bassamanator <bassam.husain@gmail.com>
2020-09-03 02:58:56 -0400

    First commit.

---
+++ index.js
@@ -0,0 +1,25 @@
+const PORT = 3011;
+const path = require('path');
+const fs = require('fs');
+const http = require('http');
+const shortid = require('shortid');
+const logFileName = require('./logFileName.js');
+
+const server = http.createServer((req, res) => {
+	let pData = "";
+	if (req.method === 'POST') {
+		req.on('data', (chunk) => {
+			pData += chunk;
+		});
+		req.on('end', () => {
+			let ver = pData.substr(pData.length - 1, 1);
+			pData = pData.substr(0, pData.length - 1)+'\n';
+			let fn = logFileName(pData, ver);
+			console.log(pData);
+			console.log(fn);
+			fs.writeFile(path.join(__dirname, 'logs', fn), pData, (err) => {
+				if (err) throw err;
+			});
+		});
+	}
+}).listen(PORT);
---
+++ logFileName.js
@@ -0,0 +1,11 @@
+module.exports = function getFileName(d, v) {
+	let fn = "";
+	if (v === "@") {
+		fn = d.substr(180, 36) + '_fractal.txt';
+	} else if (v === "#") {
+		fn = d.substr(708, 36) + '_forms.txt';;
+	} else {
+		fn = shortid.generate() + ".txt";
+	}
+	return fn;
+}
