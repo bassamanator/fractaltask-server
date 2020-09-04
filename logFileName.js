34b51c7eacf8276cb307f89c1b8e0f128a02e22b
bassamanator <bassam.husain@gmail.com>
2020-09-03 21:17:48 -0400

    Fixed logfilename.js; extra semicolon

--- logFileName.js
+++ logFileName.js
@@ -1,12 +1,12 @@
 const shortid = require('shortid');
 module.exports = function getFileName(d, v) {
-	let fn = "";
-	if (v === "@") {
-		fn = d.substr(180, 36) + '_fractal.txt';
-	} else if (v === "#") {
-		fn = d.substr(708, 36) + '_forms.txt';;
-	} else {
-		fn = shortid.generate() + ".txt";
-	}
-	return fn;
+  let fn = "";
+  if (v === "@") {
+    fn = d.substr(180, 36) + '_fractal.txt';
+  } else if (v === "#") {
+    fn = d.substr(708, 36) + '_forms.txt';
+  } else {
+    fn = shortid.generate() + ".txt";
+  }
+  return fn;
 }
